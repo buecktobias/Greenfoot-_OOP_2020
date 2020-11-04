338ab9a6425605ca7a2b8cbc69e5a16bd85a3830
buecktobias <44665917+buecktobias@users.noreply.github.com>
2020-11-04 13:11:41 +0100

    New Files 2020 4_11 lesson results rabbit can put Carrots

--- Grundbegriffe_2_OOP_v7/PlayerRabbit.java
+++ Grundbegriffe_2_OOP_v7/PlayerRabbit.java
@@ -1,21 +1,36 @@
     import greenfoot.*;
     import java.util.List;
     
-    public class PlayerRabbit extends MovingActor {
+public class PlayerRabbit extends MovingActor {
+    // KEYS
+    private final String MOVE_LEFT_KEY = "A";
+    private final String MOVE_RIGHT_KEY = "D";
+    private final String MOVE_UP_KEY = "W";
+    private final String MOVE_DOWN_KEY = "S";
+    private final String PUT_CARROT_KEY = "P";
+    
+    private final int MAX_CARROTS = 20;
+    public int currentCarrotCount = MAX_CARROTS;
     
     //Attribute
-    
+    private int life;
     
     
     //Konstruktoren
+    public PlayerRabbit(){
+       this.life = 100;
+    }
     
+    public PlayerRabbit(int newLife){
+       this.life = newLife;
+    }
     
     //Methoden
     /**
      * Wird einmal pro Zeiteinheit aufgerufen
      */
     public void act() {
-        performMovement();
+        this.listenToKeys();
     }
     
     public void setDirection(int direction){
@@ -36,19 +51,35 @@
         this.setDirection(Direction.RIGHT);
         this.move();
     }
+    
     public void moveDown(){
         this.setDirection(Direction.DOWN);
         this.move();
     }
+    
+    public void putCarrot(){
+        if (this.currentCarrotCount > 0){
+            Carrot newCarrot = new Carrot();
+            this.currentCarrotCount--;
+            this.getWorld().addObject(newCarrot, this.getX(), this.getY());
+        }
+    }
+    
+    private void listenToKeys(){
+        this.performMovement();
+        if (Greenfoot.isKeyDown(this.PUT_CARROT_KEY)){
+            this.putCarrot();
+        }
+    }
     //TODO: Steuerung über die Tasten W - A - S - D realisieren
     private void performMovement() {
-        if (Greenfoot.isKeyDown("W")) {
+        if (Greenfoot.isKeyDown(this.MOVE_UP_KEY)) {
             this.moveUp();
-        }if(Greenfoot.isKeyDown("A")){
+        }if(Greenfoot.isKeyDown(this.MOVE_LEFT_KEY)){
             this.moveLeft();
-        }if(Greenfoot.isKeyDown("D")){
+        }if(Greenfoot.isKeyDown(this.MOVE_RIGHT_KEY)){
             this.moveRight();
-        }if(Greenfoot.isKeyDown("S")){
+        }if(Greenfoot.isKeyDown(this.MOVE_DOWN_KEY)){
            this.moveDown();
         }
     }
